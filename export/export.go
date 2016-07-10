7e0e2927be1abed495f164f718da9f14d1f03795
Cosine <yzycn@qq.com>
2016-07-10 19:57:01 +0800

    export

--- export/export.go
+++ export/export.go
@@ -7,6 +7,10 @@ import (
     "log"
     "strconv"
     "strings"
+    "time"
+    "math/rand"
+    "os"
+    "fmt"
 )
 
 type DetectorDBInfo struct {
@@ -25,20 +29,18 @@ type DetectorInfo struct {
     PROVINCE_CODE  string
     CITY_CODE  string
     AREA_CODE  string
-    INSTALL_DATE  string
     EQUIPMENT_TYPE string
     LONGITUDE  string
     LATITUDE  string
-    UPLOAD_TIME_INTERVAL uint32
-    COLLECTION_RADIUS uint32
     CREATE_TIME string
-    LAST_CONNECT_TIME string
 }
 
-type DetectorInfoArr struct  {
+type DetectorInfoArr struct {
     LIST []DetectorInfo
 }
 
+var OrgCode string = "589504630"
+var OutPath = "d:/out"
 func ExportDetectorInfo() {
     session := db.GetDBSession()
     detectorArr := make([]DetectorDBInfo, 0)
@@ -52,16 +54,18 @@ func ExportDetectorInfo() {
         if len(e.Mac) < 12 {
             continue
         }
+
+        Mac := strings.ToUpper(e.Mac[len(e.Mac) - 12:])
         var detector DetectorInfo
-        detector.MAC = strings.ToUpper(e.Mac[:12])
-        detector.MAC = detector.MAC[0:2] + "-" + detector.MAC[2:4] + "-" + detector.MAC[4:6] + "-" + detector.MAC[6:8] + "-" + detector.MAC[8:10] + "-" + detector.MAC[10:12]
-        detector.EQUIPMENT_NUM = "000000000" + strings.ToUpper(e.Mac[:12])
+        detector.MAC = strings.ToUpper(e.Mac)
+        detector.MAC = Mac[0:2] + "-" + Mac[2:4] + "-" + Mac[4:6] + "-" + Mac[6:8] + "-" + Mac[8:10] + "-" + Mac[10:12]
+        detector.EQUIPMENT_NUM = OrgCode + Mac
         detector.EQUIPMENT_NAME = e.Mac
-        detector.SECURITY_FACTORY_ORGCODE = "000000000"
+        detector.SECURITY_FACTORY_ORGCODE = OrgCode
         detector.SERVICE_CODE = "00000000000000"
-        detector.PROVINCE_CODE = "000000"
-        detector.CITY_CODE = "000000"
-        detector.AREA_CODE = "000000"
+        detector.PROVINCE_CODE = "44"
+        detector.CITY_CODE = "441400"
+        detector.AREA_CODE = "441421"
         detector.EQUIPMENT_TYPE = "00"
         detector.LATITUDE = strconv.FormatFloat(e.Latitude, 'f', 6, 64)
         detector.LONGITUDE = strconv.FormatFloat(e.Longitude, 'f', 6, 64)
@@ -75,4 +79,17 @@ func ExportDetectorInfo() {
         return
     }
     log.Print(string(jsonString))
+    SaveFile(string(jsonString), "010")
+}
+
+func SaveFile(content string, typeCode string) {
+    fileName := OutPath + "/" + time.Now().Format("20060102150405") + "_" + strconv.Itoa(rand.Intn(800) + 100) + "_440200100001_" + OrgCode + "_" + typeCode +".log"
+    fout, err := os.Create(fileName)
+    defer fout.Close()
+    if err != nil {
+        fmt.Println(fileName, err)
+        return
+    }
+
+    fout.WriteString(content)
 }
