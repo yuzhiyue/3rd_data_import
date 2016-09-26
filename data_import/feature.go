3d283db2388ec5ac73f6694e4447bb43ce0292a6
cosineyu <yzycn@qq.com>
2016-09-26 18:34:04 +0800

    log path

--- data_import/feature.go
+++ data_import/feature.go
@@ -22,12 +22,15 @@ func SaveFeature(waitgroup *sync.WaitGroup, f1 Feature, f2 Feature) {
     err1 := c.Find(bson.M{"feature.value": f1.Value, "feature.type": f1.Type}).One(f1Obj)
     err2 := c.Find(bson.M{"feature.value": f2.Value, "feature.type": f2.Type}).One(f2Obj)
     if err1 == nil && err2 == nil {
-        //merge
-        feature2, ok := f2Obj["feature"]
-        if ok {
-            c.UpdateId(f1Obj["_id"].(string), bson.M{"$pushAll":bson.M{"feature":feature2.([]interface {})}})
-            c.RemoveId(f1Obj["_id"].(string))
+        if f1Obj["_id"].(string) != f2Obj["_id"].(string) {
+            //merge
+            feature2, ok := f2Obj["feature"]
+            if ok {
+                c.UpdateId(f1Obj["_id"].(string), bson.M{"$pushAll":bson.M{"feature":feature2.([]interface {})}})
+                c.RemoveId(f2Obj["_id"].(string))
+            }
         }
+
     } else if (err1 == nil && err2 != nil) {
         //push f2 in f1
         c.UpdateId(f1Obj["_id"].(string), bson.M{"$push":bson.M{"feature":bson.M{"type":f2.Type, "value":f2.Value, "org_code": f2.OrgCode, "time": f2.Time}}})
